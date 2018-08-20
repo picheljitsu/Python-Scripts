3f277124df00caf910621caa95bc6e256be2659b
Matt Pichelmayer <pichelmayer@gmail.com>
2018-08-20 17:41:17 -0500

    Update firefox_headless.py

--- firefox_headless.py
+++ firefox_headless.py
@@ -1,4 +1,17 @@
 #! /usr/bin/env python
+'''
+>>> from selenium import webdriver
+>>> from selenium.webdriver.common.keys import Keys
+>>> from selenium.webdriver.firefox.options import Options
+>>> from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+>>> 
+>>> options = Options()
+>>> options.set_headless(headless=True)
+>>> binary = FirefoxBinary('/opt/firefox/firefox')
+>>> driver = webdriver.Firefox(firefox_options=options,firefox_binary=binary)
+>>> driver.get("https://www.instagram.com/accounts/login/")
+>>> driver.title
+'''
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -30,15 +43,17 @@ class firefox_headless():
 		self.url = url
 		self.binary = FirefoxBinary(self.firefox_path)
 
+		session = webdriver.Firefox(firefox_options=self.options,firefox_binary=self.binary)
+
 		try:
-			session = webdriver.Firefox(firefox_options=self.options,firefox_binary=self.binary)
+			self.session = session
 			self.session.get(self.url)
 			self.status = True
-		 
+		 	return self.session
+
 		except:
 			self.status = False
-		
-		return 
+			return self.status
 
 	# def start_session(self, options, firefox_path):
 	# 	self.driver = webdriver.Firefox(firefox_options=options,firefox_binary=firefox_path)
@@ -46,4 +61,11 @@ class firefox_headless():
 
 if __name__ == '__main__':
 
-	#run stuff
+	msg = '''
+	Example:
+		a = firefox_headless.firefox_headless()
+		a.get_headless_url("https://www.google.com")
+		print a.session.title '''
+		
+	print(msg)
+
